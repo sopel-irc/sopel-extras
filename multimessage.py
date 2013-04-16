304a22c300a94644a0016aaf73e6df2eb5a2b5b3
Syfaro <syfaro@me.com>
2013-04-16 10:12:40 -0700

    [multimessage] initial multimessage

---
+++ multimessage.py
@@ -0,0 +1,19 @@
+"""
+multimessage.py - Send the same message to multiple users
+Copyright 2013, Syfaro Warraw http://syfaro.net
+Licensed under the Eiffel Forum License 2.
+"""
+
+def multimessage(willie, trigger):
+	"""
+    .mm <users> <message>  - Sends the same message to multiple users
+    """
+    if not trigger.isop:
+        return
+    parts = trigger.group(2).split(' ', 1)
+    nicks = parts[0].split(',')
+    for nick in nicks:
+        willie.msg(nick, parts[1])
+    willie.reply('All messages sent!')
+multimessage.commands = ['mm', 'multimessage']
+multimessage.example = '.mm nick1,nick2,nick3 my amazing message'
