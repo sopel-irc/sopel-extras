0dc5926c543e90feea213e0ec8d7d365c110ca16
Edward Powell <git@embolalia.com>
2013-11-10 17:15:01 -0500

    [document] Add configure and setup functions

--- document.py
+++ document.py
@@ -4,12 +4,47 @@ import os
 import subprocess
 
 
+def configure(config):
+    """
+    | [document] | example | purpose |
+    | ---------- | ------- | ------- |
+    | layout | default | The jekyll layout to use for the commands page |
+    | base_dir | /home/user/willie-website | The location of the jekyll site source |
+    | plugins_dir | /home/user/willie-website/_plugins | The location of the jekyll plugins directory |
+    | layouts_dir | /home/user/willie-website/_layouts | The location of the jekyll layouts directory |
+    | output_dir | /var/www/willie | The location of the jekyll generated site |
+    | jekyll_location | /opt/jekyll/jekyll | The location of the jekyll executable. Not needed if jekyll is on the user's PATH |
+    """
+    if config.option('Compile command listings to a Jekyll site', False):
+        config.add_section('document')
+        config.interactive_add('document', 'layout',
+            'The jekyll layout to use for the commands page')
+        config.interactive_add('document', 'base_dir',
+            'The location of the jekyll site source '
+        config.interactive_add('document', 'plugins_dir',
+            'The location of the jekyll plugins directory'
+        config.interactive_add('document', 'layouts_dir',
+            'The location of the jekyll layouts directory'
+        config.interactive_add('document', 'output_dir',
+            'The location of the jekyll generated site'
+        config.interactive_add('document', 'jekyll_location',
+            "The location of the jekyll executable. Not needed if jekyll is on"
+            " the user's PATH."
+
+
+def setup(bot):
+    if not bot.config.document.base_dir:
+       raise ConfigurationError('Must provide Jekyll base_dir')
+
+
 @commands('document')
 def document(bot, trigger):
-    base_dir = bot.config.document.base_dir
-    if base_dir[-1] != '/':
-        base_dir += '/'
-    layout = bot.config.document.layout or 'default'
+    conf = bot.config.document
+    layout = conf.layout or 'default'
+    base_dir = conf.base_dir
+    output_dir = conf.output_dir or os.path.join(base_dir, '_site')
+    plugins_dir = conf.plugins_dir or os.path.join(base_dir, 'plugins')
+    layouts_dir = conf.layouts_dir or os.path.join(base_dir, '_layouts')
 
     with open(os.path.join(base_dir, 'modules.md'), 'w') as f:
         front_matter = '''---\nlayout: {}\ntitle: {} commands list\n---\n\n'''
@@ -21,10 +56,9 @@ def document(bot, trigger):
             doc = bot.doc[command]
             docstring = doc[0].replace('\n\n', '<br />').replace('\n', ' ')
             f.write('| {} | {} | {} |\n'.format(command, docstring, doc[1]))
-    command = '%(x)s build -s %(b)s -d %(b)s_site -p %(b)s_plugins --layouts %(b)s_layouts'
-    command = (command %
-        {'x': bot.config.document.jekyll_location or 'jekyll',
-         'b': base_dir})
+    command = "{} build -s {} -d {} -p {} --layouts {}"
+    command = command.format(conf.jekyll_location or 'jekyll', base_dir,
+        output_dir, plugins_dir, layouts_dir)
     print command
     subprocess.call(command.split(' '))
     bot.say('Finished processing documentation.')
