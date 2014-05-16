df5103f2dc16cdb0da09138ec16d23a37b3129bb
dgw <dgw@technobabbl.es>
2014-05-15 21:08:12 -0400

    [slap] Catch IndexError if slap used in query

--- slap.py
+++ slap.py
@@ -14,7 +14,10 @@ from willie.module import commands
 def slap(willie, trigger):
     """.slap <target> - Slaps <target>"""
     text = trigger.group().split()
-    text[1] = re.sub(r"\x1f|\x02|\x12|\x0f|\x16|\x03(?:\d{1,2}(?:,\d{1,2})?)?", '', text[1])
+    try:
+        text[1] = re.sub(r"\x1f|\x02|\x12|\x0f|\x16|\x03(?:\d{1,2}(?:,\d{1,2})?)?", '', text[1])
+    except IndexError:
+        return
     if len(text) < 2 or text[1].startswith('#'):
         return
     if text[1] == willie.nick:
