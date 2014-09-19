194d60150dce0e5013ad274ccc1962413c4a68ca
dgw <dgw@technobabbl.es>
2014-09-19 07:48:23 +0400

    [slap] Slap calling user if no arg given

--- slap.py
+++ slap.py
@@ -14,11 +14,10 @@ from willie.module import commands
 def slap(willie, trigger):
     """.slap <target> - Slaps <target>"""
     text = trigger.group().split()
-    try:
-        text[1] = re.sub(r"\x1f|\x02|\x12|\x0f|\x16|\x03(?:\d{1,2}(?:,\d{1,2})?)?", '', text[1])
-    except IndexError:
-        return
-    if len(text) < 2 or text[1].startswith('#'):
+    if len(text) < 2:
+        text.append(trigger.nick)
+    text[1] = re.sub(r"\x1f|\x02|\x12|\x0f|\x16|\x03(?:\d{1,2}(?:,\d{1,2})?)?", '', text[1])
+    if text[1].startswith('#'):
         return
     if text[1] == 'me' or text[1] == 'myself':
         text[1] = trigger.nick
