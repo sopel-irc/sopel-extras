embolalia/willie-extras

[twit] Decode HTML entities in tweets
No more &lt; instead of <, etc.

twit.py
@@ -9,6 +9,7 @@ http://willie.dftba.net
 import tweepy
 import time
 import re
+import htmlentitydefs
 from willie.config import ConfigurationError
 from willie import tools
 from willie.module import rule
@@ -55,6 +56,29 @@ def tweet_url(status):
     """Returns a URL to Twitter for the given status object"""
     return 'https://twitter.com/' + status.user.screen_name + '/status/' + status.id_str
 
+def unescape(text):
+    """Returns the input with HTML entities decoded to characters"""
+    """from http://effbot.org/zone/re-sub.htm#unescape-html"""
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)
+
 @rule('.*twitter.com\/(\S*)\/status\/([\d]+).*')
 def gettweet(willie, trigger, found_match=None):
     """Show the last tweet by the given user"""
@@ -77,7 +101,7 @@ def gettweet(willie, trigger, found_match=None):
                     statusnum = int(parts[1]) - 1
                 status = api.user_timeline(twituser)[statusnum]
         twituser = '@' + status.user.screen_name
-        willie.say(twituser + ": " + unicode(status.text) + ' <' + tweet_url(status) + '>')
+        willie.say(twituser + ": " + unescape(unicode(status.text)) + ' <' + tweet_url(status) + '>')
     except:
         willie.reply("You have inputted an invalid user.")
 gettweet.commands = ['twit']
@@ -103,7 +127,7 @@ def f_info(willie, trigger):
         favourites = info.favourites_count
         followers = format_thousands(info.followers_count)
         location = info.location
-        description = info.description
+        description = unescape(info.description)
         willie.reply("@" + str(twituser) + ": " + str(name) + ". " + "ID: " + str(id) + ". Friend Count: " + friendcount + ". Followers: " + followers + ". Favourites: " + str(favourites) + ". Location: " + str(location) + ". Description: " + str(description))
     except:
         willie.reply("You have inputted an invalid user.")

sopel-irc/sopel-extras

twit.py
@@ -10,12 +10,14 @@ from __future__ import print_function
 import tweepy
 import time
 import re
+import htmlentitydefs
 from sopel.config import ConfigurationError
 from sopel import tools
 from sopel.module import rule
 import sys
 if sys.version_info.major < 3:
     str = unicode
+
 
 def configure(config):
     """
@@ -59,6 +61,29 @@ def tweet_url(status):
     """Returns a URL to Twitter for the given status object"""
     return 'https://twitter.com/' + status.user.screen_name + '/status/' + status.id_str
 
+def unescape(text):
+    """Returns the input with HTML entities decoded to characters"""
+    """from http://effbot.org/zone/re-sub.htm#unescape-html"""
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)
+
 @rule('.*twitter.com\/(\S*)\/status\/([\d]+).*')
 def gettweet(sopel, trigger, found_match=None):
     """Show the last tweet by the given user"""
@@ -81,7 +106,7 @@ def gettweet(sopel, trigger, found_match=None):
                     statusnum = int(parts[1]) - 1
                 status = api.user_timeline(twituser)[statusnum]
         twituser = '@' + status.user.screen_name
-        sopel.say(twituser + ": " + str(status.text) + ' <' + tweet_url(status) + '>')
+        sopel.say(twituser + ": " + unescape(str(status.text)) + ' <' + tweet_url(status) + '>')
     except:
         sopel.reply("You have inputted an invalid user.")
 gettweet.commands = ['twit']
@@ -107,7 +132,7 @@ def f_info(sopel, trigger):
         favourites = info.favourites_count
         followers = format_thousands(info.followers_count)
         location = info.location
-        description = info.description
+        description = unescape(info.description)
         sopel.reply("@" + str(twituser) + ": " + str(name) + ". " + "ID: " + str(id) + ". Friend Count: " + friendcount + ". Followers: " + followers + ". Favourites: " + str(favourites) + ". Location: " + str(location) + ". Description: " + str(description))
     except:
         sopel.reply("You have inputted an invalid user.")